cb8bf2a396009d7c03cd99bbe8c8ac3e2fcdefa6
soyoung <soyseo99@naver.com>
2023-12-03 11:54:49 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,47 +8,50 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="main-banner"></div>
+        <div className="main-banner">
+          <span></span>
+          <span></span>
+        </div>
         <div className="main-items">
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>크림 드로우</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>실시간 차트</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>남성 추천</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>여성 추천</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>추가 10%쿠폰</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>정가 아래</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>선데이오프클럽</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>인기 롱패딩</a>
           </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
-          </div>
+            <a>12월 베네핏</a>
+            </div>
           <div className="main-item">
             <div className="main-item__box"></div>
-            <a>크림드로우</a>
+            <a>스타일컬렉터 모집</a>
           </div>
         </div>
       </div>
