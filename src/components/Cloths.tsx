b9c7ff0551c8c00e554d74911f96f75e9a625673
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-07 05:31:17 +0000

    add flex-wrap, space-between

--- src/components/Cloths.tsx
+++ src/components/Cloths.tsx
@@ -1,13 +1,14 @@
 import Cloth from "./Cloth";
+import styles from "./Cloths.module.css";
 function Cloths() {
   return (
-    <>
+    <div className={styles.cloths}>
       <Cloth />
       <Cloth />
       <Cloth />
       <Cloth />
       <Cloth />
-    </>
+    </div>
   );
 }
 
