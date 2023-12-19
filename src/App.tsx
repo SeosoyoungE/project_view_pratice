f9cba7f7288994b26154e33178ee7c71f19b2a4e
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-19 00:56:43 +0000

    헤더 라우터 처리

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Header from "./routes/Header";
 import Footer from "./routes/Foot";
 import Recommend from "./routes/Recommend";
 
+import "./index.css";
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Recommend />} />
         <Route path="/lank" element={<h1>lank</h1>} />
+        <Route path="/luxury" element={<h1>luxury</h1>} />
+        <Route path="/man" element={<h1>man</h1>} />
+        <Route path="/woman" element={<h1>woman</h1>} />
+        <Route path="/discovery" element={<h1>discovery</h1>} />
+        <Route path="*" element={<h1>없는페이지 입니다.</h1>} />
       </Routes>
       <Footer />
     </div>
